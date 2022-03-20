f2f516434525d38a1258b87d4dfaf16afb1ce7c5
Emperorfizzy01 <farukgbolahan21@gmail.com>
2022-03-20 11:39:55 +0100

    First commit

--- android/app/src/main/java/co/appbrewery/magic8ball/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/magic8ball/MainActivity.java
@@ -1,13 +1,14 @@
 package co.appbrewery.magic8ball;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      MaterialApp(home: magicBall()),
+    );
+
+class magicBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Ask Me Anything'),
+        ),
+        body: Ball());
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int BallNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            BallNumber = Random().nextInt(5) + 1;
+            print('Clicked');
+          });
+        },
+        child: Image.asset('images/ball$BallNumber.png'),
       ),
     );
+  }
+}
